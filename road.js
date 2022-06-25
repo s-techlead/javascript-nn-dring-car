577c1520660495e118dcb27da5156e037c76cd5b
s-techlead <sebastienschnd.techlead@gmail.com>
2022-06-25 14:08:17 +0200

    Start road drawing

---
+++ road.js
@@ -0,0 +1,34 @@
+class Road{
+    constructor(x,width,laneCount=3) {
+        this.x=x;
+        this.width=width;
+        this.laneCount=laneCount;
+
+        this.left=x-width/2;
+        this.right=x+width/2;
+
+        const infinity=1000000;
+        this.top=-infinity;
+        this.botton=infinity;
+    }
+
+    draw(ctx){
+        ctx.lineWidth=5;
+        ctx.strokeStyle="white";
+
+        for(let i=0; i<=this.laneCount;i++) {
+            const x=lerp(
+                this.left,
+                this.right,
+                i/this.laneCount
+            );
+            ctx.beginPath();
+            ctx.moveTo(x, this.top);
+            ctx.lineTo(x, this.botton)
+            ctx.stroke();
+        }
+    }
+}
+
+
+
